9a0f325676915a6b52ed5875d79f226f5cdbe06d
louiserondia <louiserondia@gmail.com>
2025-07-15 00:49:35 +0200

    tape

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,8 @@
-let () = print_endline "Hello, World!"
+let () =
+  let tape = Turing.Tape.init '.' in
+  let tape = Turing.Tape.add_right tape 'y' in 
+  let tape = Turing.Tape.move_right tape in 
+  let tape = Turing.Tape.add_right tape 'o' in 
+  let tape = Turing.Tape.move_max_left tape in 
+let s = String.of_seq (List.to_seq (Turing.Tape.to_list tape)) in
+print_endline s
---
+++ lib/turing.ml
@@ -0,0 +1,37 @@
+module Tape = struct
+  type tape = { left : char list; right : char list; v : char }
+
+  let init v = { left = []; right = []; v }
+
+  let move_left tape =
+    match tape.left with
+    | [] -> tape
+    | l_hd :: l_tl -> { left = l_tl; v = l_hd; right = tape.v :: tape.right }
+
+  let move_right tape =
+    match tape.right with
+    | [] -> tape
+    | r_hd :: r_tl -> { left = tape.v :: tape.left; v = r_hd; right = r_tl }
+
+  let add_left tape v =
+    match tape with
+    | { left = []; _ } -> { tape with left = v :: tape.left }
+    | _ -> assert false
+
+  let add_right tape v =
+    match tape with
+    | { right = []; _ } -> { tape with right = v :: tape.right }
+    | _ -> assert false
+
+  let rec move_max_left tape =
+    match tape with
+    | { left = []; _ } -> tape
+    | _ -> move_max_left (move_left tape)
+
+  let rec move_max_right tape =
+    match tape with
+    | { right = []; _ } -> tape
+    | _ -> move_max_right (move_right tape)
+
+  let to_list tape = (tape.left |> List.rev) @ [ tape.v ] @ tape.right
+end
